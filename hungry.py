1f3a54939a9b9d5d4493982848e33b4f639acbb3
FatmaAbdeldjouad <fatma.abdeldjouad@gmail.com>
2022-06-14 22:47:48 +0100

    add yo

--- hungry.py
+++ hungry.py
@@ -1,8 +1,7 @@
 hungry=input("Are you hungry?")
-if hungry=="yes":
+if hungry=="yes" and hungry=="yo":
     print("eat mehadjeb :)")
     print("eat pizza")
-    print("eat fries")
 else:
     thirsty=input("Are you thirsty?")
     if thirsty=='yes':
